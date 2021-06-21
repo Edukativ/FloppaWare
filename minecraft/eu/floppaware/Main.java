01a7f46a89a9f0cdccf70b5a9cd3d3a7e20925c7
MyLifeIsTrash <80705302+MyLifeIsTrash@users.noreply.github.com>
2021-06-21 14:35:55 +0300

    6

--- minecraft/eu/floppaware/Main.java
+++ minecraft/eu/floppaware/Main.java
@@ -5,6 +5,7 @@ import eu.floppaware.event.EventTarget;
 import eu.floppaware.event.events.impl.input.EventKey;
 import eu.floppaware.feature.Feature;
 import eu.floppaware.feature.FeatureManager;
+import eu.floppaware.ui.click.ClickGui;
 
 public class Main {
 
@@ -12,9 +13,11 @@ public class Main {
 
     public static Main instance = new Main();
     public FeatureManager featureManager;
+    public ClickGui clickGui;
 
     public void loadClient() {
         this.featureManager = new FeatureManager();
+        this.clickGui = new ClickGui();
         EventManager.register(this);
     }
 
---
+++ minecraft/eu/floppaware/feature/impl/hud/ClickGui.java
@@ -0,0 +1,21 @@
+package eu.floppaware.feature.impl.hud;
+
+import eu.floppaware.Main;
+import eu.floppaware.feature.Category;
+import eu.floppaware.feature.Feature;
+import org.lwjgl.input.Keyboard;
+
+public class ClickGui extends Feature {
+
+    public ClickGui() {
+        super("ClickGui", Category.Hud);
+        setKey(Keyboard.KEY_RSHIFT);
+    }
+
+    @Override
+    public void onEnable() {
+        mc.displayGuiScreen(Main.instance.clickGui);
+        state();
+        super.onEnable();
+    }
+}
