130df8dfb87da24013eb4e863f52e57d64661eb9
Edukativ <86229739+Edukativ@users.noreply.github.com>
2021-06-21 12:19:39 +0300

    Update Main.java

--- minecraft/eu/floppaware/Main.java
+++ minecraft/eu/floppaware/Main.java
@@ -8,15 +8,14 @@ import eu.floppaware.feature.FeatureManager;
 
 public class Main {
 
-    public static String name = "МАТЬ РУСЛАНА ШЛЮХА ЕБАННАЯ", build = "001", status = "Alpha";
+    public static String name = "FloppaWare", build = "001", status = "Alpha";
 
     public static Main instance = new Main();
-    public FeatureManager featureManager;//сын шлюхи ебанной гетни воспитание  да я твоей матери в ебало рыгнул
+    public FeatureManager featureManager;
 
     public void loadClient() {
         this.featureManager = new FeatureManager();
         EventManager.register(this);
-        //мать руслана шлюха ушастая
     }
 
     @EventTarget
